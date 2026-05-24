69d7b442331d5565ad7a63099983c1f6d8e095a7
Ahmad PointCraft <ahmad.elshowair@point-craft.com>
2026-05-24 08:31:33 +0700

    Updates

---
+++ sw.js
@@ -0,0 +1,57 @@
+var CACHE_NAME = 'masjid-v1';
+
+var PRECACHE_URLS = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(PRECACHE_URLS);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      return Promise.all(
+        names
+          .filter(function (name) { return name !== CACHE_NAME; })
+          .map(function (name) { return caches.delete(name); })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  var url = new URL(event.request.url);
+  var isStaticAsset = /\.(js|css|jpg|jpeg|png|svg|woff2)$/i.test(url.pathname);
+
+  if (isStaticAsset) {
+    event.respondWith(
+      caches.match(event.request).then(function (cached) {
+        return cached || fetch(event.request).then(function (response) {
+          if (response.ok) {
+            var clone = response.clone();
+            caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(event.request, clone);
+            });
+          }
+          return response;
+        });
+      })
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request).catch(function () {
+        return caches.match(event.request);
+      })
+    );
+  }
+});
